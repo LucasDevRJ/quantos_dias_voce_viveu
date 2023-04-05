6531f1de50be9f41e95ca735907ab1dcd345f31d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-05 13:41:25 -0300

    Função que calcula a quantidade de dias vividos

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,10 @@
 function calculaDiasVividos() {
 	var campoIdade = document.querySelector("input");
+	var resposta = document.querySelector("h2");
 
 	var idade = campoIdade.value;
 
-	console.log(idade);
+	var diasVividos = idade * 365;
+
+	resposta.textContent = "Você já viveu " + diasVividos + " dias!!!";
 }
